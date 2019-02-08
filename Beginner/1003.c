b6c4ab3bb147bf54fca6ecdd6c60568baaf3dbc2
José Guilherme Fernandes Moura <joseguilhermefmoura@gmail.com>
2019-02-08 20:27:10 -0200

    Create 1003.c

---
+++ Beginner/1003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	//same logic as 1001
+	int number1, number2, sum;
+	scanf("%i", &number1);
+	scanf("%i", &number2);
+  
+	sum = number1 + number2;
+	printf("SOMA = %i\n", sum);
+	return 0;
+}
