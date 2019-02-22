6b8cdbe63ed9321eeb3d81019e48c4615f79bfa0
José Guilherme Fernandes Moura <joseguilhermefmoura@gmail.com>
2019-02-22 16:52:09 -0300

    Create 1004.c

---
+++ Beginner/1004.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+	int number1, number2, prod; 
+
+	scanf("%i", &number1);
+	scanf("%i", &number2);
+
+	prod = number1 * number2;
+
+	printf("PROD = %d\n", prod);
+
+	return 0; 
+}
